9d86662b6a5e7f1e6d58bfe7454075788edbbe54
Oliver Foggin <oliverfoggin@me.com>
2021-09-24 15:06:12 +0100

    Initial Commit

---
+++ BrokenImagePickers/BrokenImagePickersApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrokenImagePickersApp.swift
+//  BrokenImagePickers
+//
+//  Created by Foggin, Oliver (Developer) on 24/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct BrokenImagePickersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
